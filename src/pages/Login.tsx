e16bcf4dc95e1f335dc48ca8cdaae92c313a8ad7
fai-sas <craftedbyfaisal@gmail.com>
2024-07-04 19:34:00 +0600

    login page added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,29 @@
+import { Button } from 'antd'
+import { useForm } from 'react-hook-form'
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: 'A-0002',
+      password: 'admin123',
+    },
+  })
+
+  const onSubmit = async (data) => {
+    console.log(data)
+  }
   return (
-    <div>
-      <h1> This is Login component </h1>
-    </div>
+    <form className='p-12' onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor='id'>ID: </label>
+        <input type='text' id='id' {...register('userId')} />
+      </div>
+      <div>
+        <label htmlFor='password'>Password: </label>
+        <input type='text' id='password' {...register('password')} />
+      </div>
+      <Button htmlType='submit'>Login</Button>
+    </form>
   )
 }
 
