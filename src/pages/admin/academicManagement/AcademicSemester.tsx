2275c303289f4b24919275e388a7e18eb67e337d
fai-sas <craftedbyfaisal@gmail.com>
2024-07-06 19:37:47 +0600

    admin routes updated

---
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -0,0 +1,13 @@
+const AcademicSemester = () => {
+  // const { data } = useGetAllSemestersQuery(undefined)
+
+  // console.log(data)
+
+  return (
+    <div>
+      <h1> This is AcademicSemester component </h1>
+    </div>
+  )
+}
+
+export default AcademicSemester
